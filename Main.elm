f8ea36b6be62e18ef2f02aae789f31b7956f22f8
tazzo <roberto.tazzoli@gmail.com>
2017-05-09 21:48:11 +0200

    comment removed

--- Main.elm
+++ Main.elm
@@ -1,9 +1,3 @@
-{- This file re-implements the Elm Counter example (1 counter) with elm-mdl
-   buttons. Use this as a starting point for using elm-mdl components in your own
-   app.
--}
-
-
 module Main exposing (..)
 
 import Html exposing (..)
