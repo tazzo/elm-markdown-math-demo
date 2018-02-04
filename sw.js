66111fd7b5e349a6141e250db9130557a7cfef3b
tazzo <roberto.tazzoli@gmail.com>
2018-02-04 01:30:40 +0100

    serviceworker first try

---
+++ sw.js
@@ -0,0 +1,18 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+workbox.precaching.precacheAndRoute([
+    '/index.html',
+    '/elm-main.js',
+    '/dist/*.png',
+]);
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  workbox.strategies.networkFirst()
+);
